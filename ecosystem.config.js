310063d86b64d7ba0b96d1ffb5b419b881386404
ahmedWessamTest <ahmedwessam202@gmail.com>
2025-12-22 16:21:30 +0200

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,54 +2,13 @@ module.exports = {
   apps: [
     {
       name: "meditrade-ssr",
-
-      // Entry file
       script: "./server/server.mjs",
-
-      // ===============================
-      // Performance
-      // ===============================
-      exec_mode: "fork",          // Fork mode
-      // instances غير مطلوب في fork mode
-
-      node_args: "--max-old-space-size=2048",
-
-      // ===============================
-      // Environment
-      // ===============================
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
         PORT: 80,
         HOST: "127.0.0.1"
       },
-
-      // ===============================
-      // Logs
-      // ===============================
-      out_file: "/home/meditradeeg/logs/ssr-out.log",
-      error_file: "/home/meditradeeg/logs/ssr-error.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-
-      // ===============================
-      // Stability
-      // ===============================
-      max_memory_restart: "800M",
-      restart_delay: 3000,
-      kill_timeout: 5000,
-      listen_timeout: 10000,
-
-      // ===============================
-      // Reliability
-      // ===============================
-      autorestart: true,
-      watch: false,
-      time: true,
-
-      // ===============================
-      // Advanced
-      // ===============================
-      merge_logs: true,
-      instance_var: "PM2_INSTANCE_ID"
     }
   ]
 };
