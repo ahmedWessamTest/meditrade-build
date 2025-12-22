8b5a87be2e30a6e6b23082471bbc0c5190a24f9f
ahmedWessamTest <ahmedwessam202@gmail.com>
2025-12-22 18:29:33 +0200

    update server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: "meditrade-ssr",
-      script: "./server/server.mjs",
-      exec_mode: "fork",
+      name: 'medi-trade-live',
+      script: 'server/server.mjs',
+      instances: 'max',
+      exec_mode: 'cluster',
+      interpreter: 'node',
+      node_args: '--optimize_for_size --max_old_space_size=2048',
+      max_memory_restart: '1G',
+      restart_delay: 4000,
       env: {
-        NODE_ENV: "production",
-        PORT: 80,
-        HOST: "127.0.0.1"
+        NODE_ENV: 'development',
+        PORT: 4000
       },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4000,
+        SSR_CACHE_TTL_SECONDS: '600',
+        SSR_REDIS_PREFIX: 'ssr:'
+      }
     }
   ]
 };
